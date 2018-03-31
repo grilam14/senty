f6b13db333d2f90f0f69269e9753b84fd83122b2
jonathanbayer <jonathan.bayer@colorado.edu>
2018-03-31 13:07:29 -0600

    updated with SQL integration

--- SentimentAnalysis/bot_detect.py
+++ SentimentAnalysis/bot_detect.py
@@ -1,8 +1,7 @@
 import json
 import botometer
-# the botometer library must be install by running the command:
-# pip install botometer
-# int the terminal in order for this method to function
+from config import *
+import mysql.connector
 
 
 # acc_name is the @accountname or account ID (from tweet data)
@@ -12,17 +11,29 @@ import botometer
 # values indicate a higher likelihood of the account being a bot
 
 def bot_detector(acc_name):
-	# keys acquired from mashape marketplace and twitter's developer program
-	mashape_key = 'Qd8Lh3avPDmshLjJIeQzGyr2qHRcp1mSjbwjsnv4QJO3fUlQ1p'
-	twitter_app_auth = {
-		'access_token': '2852131812-dIlDgWIm65Xi5cKiDGgK3nsaRBYcdjH2JJ4hZYL',
-		'access_secret': 'uGHDZuS3uDaION8GnHwlOg3nCtzF3t5OrOJhSAoP7R4Of',
-		'consumer_key': '5S9D6TlASa3I4B5IkMYlQ3KWX',
-		'consumer_secret': 'ruxsdIseFAdT35lt0dF97hotZ3w89AdyZURI4Mv6dOKYHcnFmw',
-	}
+	mashape_key = MASHAPE_KEY
+	twitter_app_auth = TWITTER_AUTH
+	db = mysql.connector.connect(**SQLconfig)
 
-	meter = botometer.Botometer(mashape_key = mashape_key, **twitter_app_auth)
+	cursor = db.cursor()
+	query = "SELECT is_bot FROM accounts WHERE name = " + "'" + acc_name + "'"
+	cursor.execute(query)
+	result = cursor.fetchone()
 
-	acc_score = json.dumps(meter.check_account(acc_name))
-	return json.loads(acc_score.strip())['scores']['universal']
+	if result is None: #account not yet in database, get score and add to DB
+		meter = botometer.Botometer(mashape_key = mashape_key, **twitter_app_auth)
 
+		acc_score = json.dumps(meter.check_account(acc_name))
+		acc_score = json.loads(acc_score.strip())['scores']['universal']
+
+		if float(acc_score) < 0.66:
+			is_bot = 0
+		else:
+			is_bot = 1
+
+		add_acc = "INSERT INTO accounts (name, is_bot) VALUES(%s,%s)"
+		cursor.execute(add_acc, (acc_name, is_bot))
+		db.commit()
+		return is_bot
+	else: #account already checked, return pre-determined bot score
+		return result[0]
