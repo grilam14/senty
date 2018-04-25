fa73d9cc0e5b82cc80115400396e623770da9352
Andrew <andrew.grillot@gmail.com>
2018-04-24 19:10:01 -0600

    Update web.py

--- ProjectWebsite/web.py
+++ ProjectWebsite/web.py
@@ -20,10 +20,6 @@ mysql.init_app(app)
 
 
 
-@app.route("/")
-def main():
-     return(render_template('Senty.html'))
-
 @app.route("/", methods= ['GET','POST'])
 def index():
     if request.method == 'POST':
@@ -81,43 +77,42 @@ def validateLogin():
         con.close()
     
 
-@app.route('/showSignUp')
-def showSignUp():
-
-    return (render_template('signup.html'))
-    
-
 
 @app.route('/signUp',methods=['POST','GET'])
 def signUp():
+    
     # still need to create signup class and transfer below code to new file
-    try:
+    conn = mysql.connect()
+    cur = conn.cursor()
+    #code=307 runs homepage without updating the actual page your on. So weird. 
+    #return redirect(url_for('main'))
+
+    if request.method == 'POST':
         _name = request.form['inputName']
         _email = request.form['inputEmail']
         _password = request.form['inputPassword']
 
         # validate the received values
         if _name and _email and _password:
+            print(_name, _email, _password)
             
-            conn = mysql.connect()
-            cur = conn.cursor()
             cur.callproc('sp_createUser',(_name,_email,_password,))
-            print "Registered"
+            print ("Registered")
             data = cur.fetchall()
 
-            if len(data) is 0:
-                conn.commit()
-                return(json.dumps({'message':'User created successfully !'}))
-            else:
-                return(json.dumps({'error':str(data[0])}))
+            conn.commit()
+            cur.close() 
+            conn.close()
+            json.dumps({'message':'User created successfully !'})
+            print('redirecting')
+            return redirect(url_for('index'))
+
+
         else:
             return json.dumps({'html':'<span>Enter the required fields</span>'})
 
-    except Exception as e:
-        return json.dumps({'error':str(e)})
-    finally:
-        cur.close() 
-        conn.close()
+    else:
+        return render_template('signup.html')
 
 
 if __name__ == "__main__":
