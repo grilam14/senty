d4305f77466017e61e6eaaa822c7c7c4e9807147
mmoghadam1 <35235767+mmoghadam1@users.noreply.github.com>
2018-04-24 21:19:17 -0600

    Update project.sql

--- ProjectWebsite/SQL/project.sql
+++ ProjectWebsite/SQL/project.sql
@@ -46,3 +46,14 @@ BEGIN
     END IF;
 END$$
 DELIMITER ;
+
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_validateLogin`(
+IN p_username VARCHAR(20)
+)
+BEGIN
+    select * from tbl_user where user_username = p_username;
+END$$
+DELIMITER ;
+
